bf838a9beb673e141d628aae586984443aa4d21b
Łukasz Knop <Byczu95@users.noreply.github.com>
2017-03-28 21:11:48 +0200

    Displaying MAC Adresses from .pcap files

--- PT_UI_Design/PT_UI_Design/PcapFileControl.xaml.cs
+++ PT_UI_Design/PT_UI_Design/PcapFileControl.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SharpPcap;
+using PacketDotNet;
+using SharpPcap.LibPcap;
+using NUnit.Framework;
 
 namespace PT_UI_Design
 {
@@ -21,12 +25,61 @@ namespace PT_UI_Design
     public partial class PcapFileControl : UserControl
     {
         private string _filePath;
-
+        private static Label _label;
         public PcapFileControl(string filePath)
         {
             InitializeComponent();
+            _label = label;
             _filePath = filePath;
-            label.Content = _filePath;
+
+            ICaptureDevice device;
+
+            try
+            {
+                // Get an offline device
+                device = new CaptureFileReaderDevice(_filePath);
+
+                // Open the device
+                device.Open();
+            }
+            catch (Exception e)
+            {
+                label.Content += "Caught exception when opening file" + e.ToString();
+                return;
+            }
+
+            device.OnPacketArrival += new PacketArrivalEventHandler(device_OnPacketArrival);
+            label.Content += "-- Capturing from " +  _filePath + '\n';
+
+            device.Capture();
+
+            device.Close();
+            label.Content += "-- End of file reached.";
+            
         }
+
+        private static int packetIndex = 0;
+
+        /// <summary>
+        /// Prints the source and dest MAC addresses of each received Ethernet frame
+        /// </summary>
+        private static void device_OnPacketArrival(object sender, CaptureEventArgs e)
+        {
+            if (e.Packet.LinkLayerType == PacketDotNet.LinkLayers.Ethernet)
+            {
+                var packet = PacketDotNet.Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
+                var ethernetPacket = (PacketDotNet.EthernetPacket)packet;
+
+                _label.Content += packetIndex + "At: ";
+                _label.Content += e.Packet.Timeval.Date.ToString() + " : ";
+                _label.Content += e.Packet.Timeval.Date.Millisecond + " : MAC: ";
+                _label.Content += ethernetPacket.SourceHwAddress + " -> MAC: ";
+                _label.Content += ethernetPacket.DestinationHwAddress.ToString() + "\n";
+                
+                packetIndex++;
+            }
+            
+        }
+
     }
 }
