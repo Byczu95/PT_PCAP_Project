dad629248f1ed509d409b5d45989f4b934f3f90d
Łukasz Knop <Byczu95@users.noreply.github.com>
2017-04-26 19:56:49 +0200

    Added merging packet lits from multiple files

--- PT_UI_Design/PT_UI_Design/PcapFileControl.xaml.cs
+++ PT_UI_Design/PT_UI_Design/PcapFileControl.xaml.cs
@@ -134,22 +134,231 @@ namespace PT_MAPACKET
                     case "Adres źródłowy IP":
                         if (myP.SourceIP == cleanFilterText)
                             return true;
-                        else
-                            return false;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SharpPcap;
+using PacketDotNet;
+using SharpPcap.LibPcap;
+using PT_UI_Design.Models;
+using NUnit.Framework;
 
-                    case "Adres docelowy IP":
-                        if (myP.SourceMac == cleanFilterText)
-                            return true;
-                        else
-                            return false;
-                    default:
-                        return false; //packet is not OK
-                }      
+namespace PT_UI_Design
+{
+    /// <summary>
+    /// Interaction logic for PcapFileControl.xaml
+    /// </summary>
+    public partial class PcapFileControl : UserControl, IGetPacket
+    {
+        private string _filePath;
+        private static TextBox _textBox;
+        private static ListView _listview;
+        public static List<MyPacket> packets;
+        public static int a;
+
+        public PcapFileControl()
+        {
+            InitializeComponent();
+            _textBox = textBox;
+            _listview = listViewFileData;
+            //_filePath = filePath;
+            packets = new List<MyPacket>();
+
+            filterComboBox.Items.Add("Adres źródłowy MAC");
+            filterComboBox.Items.Add("Adres docelowy MAC");
+            filterComboBox.Items.Add("Adres źródłowy IP");
+            filterComboBox.Items.Add("Adres docelowy IP");
+
+            ICaptureDevice device;
         }
+        public PcapFileControl(string filePath)
+        {
+            InitializeComponent();
+            _textBox = textBox;
+            _listview = listViewFileData;
+            _filePath = filePath;
+            packets = new List<MyPacket>();
+
+            filterComboBox.Items.Add("Adres źródłowy MAC");
+            filterComboBox.Items.Add("Adres docelowy MAC");
+            filterComboBox.Items.Add("Adres źródłowy IP");
+            filterComboBox.Items.Add("Adres docelowy IP");
+
+            ICaptureDevice device;
+
+            try
+            {
+                // Get an offline device
+                device = new CaptureFileReaderDevice(_filePath);
+
+                // Open the device
+                device.Open();
+            }
+            catch (Exception e)
+            {
+                textBox.Text += "Caught exception when opening file" + e.ToString();
+                return;
+            }
+
+            device.OnPacketArrival += new PacketArrivalEventHandler(device_OnPacketArrival);
+            textBox.Text += "-- Capturing from " +  _filePath + "\n\n";
+
+            device.Capture();
+
+            device.Close();
+            packetIndex = 1;
+            
+        }
+        public PcapFileControl(List<MyPacket> Files)
+        {
+            InitializeComponent();
+            _textBox = textBox;
+            _listview = listViewFileData;
+            //_filePath = filePath;
+            int packetIndex = 1;
+            packets = new List<MyPacket>();
+
+            filterComboBox.Items.Add("Adres źródłowy MAC");
+            filterComboBox.Items.Add("Adres docelowy MAC");
+            filterComboBox.Items.Add("Adres źródłowy IP");
+            filterComboBox.Items.Add("Adres docelowy IP");
+
+            ICaptureDevice device;
+
+            textBox.Text = "Merged file";
+
+            
+                foreach (MyPacket myPacket in Files)
+                {
+
+                    _listview.Items.Add(new MyPacket
+                    {
+                        Id = packetIndex,
+                        Time = myPacket.Time,
+                        SourceIP = myPacket.SourceIP,
+                        DestIP = myPacket.DestIP,
+                        SourceMac = myPacket.SourceMac,
+                        DestMac = myPacket.DestMac
+                    });
+
+                    packets.Add(new MyPacket
+                    {
+                        Id = packetIndex,
+                        Time = myPacket.Time,
+                        SourceIP = myPacket.SourceIP,
+                        DestIP = myPacket.DestIP,
+                        SourceMac = myPacket.SourceMac,
+                        DestMac = myPacket.DestMac
+                    });
+
+                    packetIndex++;
+                }
+        }
+
+        private static int packetIndex = 1;
+
+        /// <summary>
+        /// Prints the source and dest IP and MAC addresses of each received Ethernet frame
+        /// </summary>
+        private static void device_OnPacketArrival(object sender, CaptureEventArgs e)
+        {
+            if (e.Packet.LinkLayerType == PacketDotNet.LinkLayers.Ethernet)
+            {
+                var packet = PacketDotNet.Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
+
+                if ((PacketDotNet.EthernetPacket)packet != null)
+                {
+                    var ethernetPacket = (PacketDotNet.EthernetPacket)packet;
+
+                    if (IpPacket.GetEncapsulated(packet) != null)
+                    {
+                        var ipPacket = IpPacket.GetEncapsulated(packet);
+
+                        _listview.Items.Add(new MyPacket { Id = packetIndex, 
+                                                         Time = e.Packet.Timeval.Date.ToString(),
+                                                         SourceIP = ipPacket.SourceAddress.MapToIPv4().ToString(), 
+                                                         DestIP = ipPacket.DestinationAddress.MapToIPv4().ToString(),
+                                                         SourceMac = ethernetPacket.SourceHwAddress.ToString(),
+                                                         DestMac = ethernetPacket.DestinationHwAddress.ToString()
+                                                            });
+                        packets.Add(new MyPacket
+                        {
+                            Id = packetIndex,
+                            Time = e.Packet.Timeval.Date.ToString(),
+                            SourceIP = ipPacket.SourceAddress.MapToIPv4().ToString(),
+                            DestIP = ipPacket.DestinationAddress.MapToIPv4().ToString(),
+                            SourceMac = ethernetPacket.SourceHwAddress.ToString(),
+                            DestMac = ethernetPacket.DestinationHwAddress.ToString()
+                        });
+                        packetIndex++;
+                    }
+                }
+                
+            }
+        }
+
+        public List<MyPacket> getPacketsData()
+        {
+            return packets;
+        }
+
+        public List<MyPacket> getPackets()
+        {
+            return packets;
+        }
+
+
+        private bool packetIsGoodForFilter(MyPacket myP)
+        {
+            //tu tzeba czyscic filterTextBox.Text i bd dzialac
+            string cleanFilterText = filterTextBox.Text;
+
+            switch (filterComboBox.SelectedValue.ToString())
+            {
+                case "Adres źródłowy MAC":
+                    if (myP.SourceMac == cleanFilterText)
+                        return true;
+                    else
+                        return false;
+
+                case "Adres docelowy MAC":
+                    if (myP.DestMac == cleanFilterText)
+                        return true;
+                    else
+                        return false;
+
+                case "Adres źródłowy IP":
+                    if (myP.SourceIP == cleanFilterText)
+                        return true;
+                    else
+                        return false;
+
+                case "Adres docelowy IP":
+                    if (myP.SourceMac == cleanFilterText)
+                        return true;
+                    else
+                        return false;
+                default:
+                    return false; //packet is not OK
+            }
+        }
+
+
 
         private void set_listview(List<MyPacket> myList)
         {
-            foreach(MyPacket elem in myList)
+            foreach (MyPacket elem in myList)
                 _listview.Items.Add(elem);
         }
 
@@ -164,6 +373,7 @@ namespace PT_MAPACKET
             else
                 filterTextBox.IsEnabled = true;
         }
+
 
         private void filterButton_Click(object sender, RoutedEventArgs e)
         {
@@ -182,6 +392,7 @@ namespace PT_MAPACKET
 
             set_listview(viewPackets);
         }
+
     }
 
     public interface IGetPacket
