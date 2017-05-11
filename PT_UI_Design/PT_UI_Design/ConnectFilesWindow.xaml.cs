ba52ce037db7ac946d347a78d06a2b36ad8b8fe4
Łukasz Knop <Byczu95@users.noreply.github.com>
2017-05-11 12:04:46 +0200

    Deleting duplicates of packets if checkbox is selected

--- PT_UI_Design/PT_UI_Design/ConnectFilesWindow.xaml.cs
+++ PT_UI_Design/PT_UI_Design/ConnectFilesWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace PT_MAPACKET
             InitializeComponent();
             this._tabItems = tabItems;
 
-            foreach(TabItem elem in _tabItems)
+            foreach (TabItem elem in _tabItems)
             {
                 if (IsAPcapFile(elem.Header.ToString()))
                 {
@@ -61,7 +61,8 @@ namespace PT_MAPACKET
         {
             PcapFileControl pcapWindow = new PcapFileControl();
             List<MyPacket> packets = new List<MyPacket>();
-
+            int i = 0;
+            bool isDuplicate = false;
             foreach (TabItem elem in _tabItems)
             {
                 if (ListViewCFW.Items.Contains(elem.Header))
@@ -69,26 +70,51 @@ namespace PT_MAPACKET
                     if (IsAPcapFile(elem.Header.ToString()))
                     {
                         pcapWindow = (PcapFileControl)elem.Content;
-                        foreach(MyPacket p in pcapWindow.listViewFileData.Items)
+                        foreach (MyPacket p in pcapWindow.listViewFileData.Items)
                         {
                             if (this.deleteDuplicate.IsChecked == true)
                             {
-                                //TODO
-                                packets.Add(p);
+                                if (i > 1) // tu powinno być zero, ale licznik przeskoczy do 1 przez nowo utworzony merged.pcap
+                                {
+                                    if (packets.Count > 0)
+                                    {
+                                        foreach (MyPacket listPacket in packets)
+                                        {
+                                            if (p.SourceIP.Equals(listPacket.DestIP) && p.Checksum.Equals(listPacket.Checksum))  //sprawdzam czy dany pakiet ma już swój duplikat na liście
+                                            {
+                                                isDuplicate = true;
+                                            }
+                                        }
+                                        if (isDuplicate == false) //dodaję pakiet jeśli jest unikalny
+                                        {
+                                            packets.Add(p);
+                                        }
+                                        
+                                    } //jeśli na liście nie ma obiektów
+                                    else
+                                    {
+                                        packets.Add(p);
+                                    }
+                                }
+                                else  //uzupełnij listę pakietami z pierwszego pliku
+                                {
+                                    packets.Add(p);
+                                }
                             }
-                            else
+                            else  // gdy checkbox nie jest zaznaczony
                             {
                                 packets.Add(p);
                             }
                         }
+                        i++;
                     }
                 }
             }
-            
+
             PcapFileControl pfc = new PcapFileControl(packets);
             _tabItems[0].Content = pfc;
 
-            return true; 
+            return true;
         }
     }
 
